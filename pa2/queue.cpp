45d8e9d6d23c0747159cedd7f9689634087edbe8
UBC Student <hdhali01@annacis.students.cs.ubc.ca>
2021-02-28 22:34:33 -0800

    test

--- pa2/queue.cpp
+++ pa2/queue.cpp
@@ -24,7 +24,7 @@ void Queue<T>::enqueue(T newItem)
 template <class T>
 T Queue<T>::dequeue()
 {
-    return myQueue.popL();
+    return myQueue.popR();
 }
 
 
@@ -37,7 +37,7 @@ T Queue<T>::dequeue()
 template <class T>
 T Queue<T>::peek()
 {
-    return myQueue.peekL();
+    return myQueue.peekR();
 }
 
 /**
